9964cfbbbc7f00184b858485757664c6eb269428
Alexander <shalagin.aiu@phystech.edu>
2024-03-13 03:14:16 +0300

    Добавьте файлы проекта.

---
+++ 4_2_1/4_2_1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+template<typename Container>
+void print(Container& cont, char c) {
+
+	for (auto i = cont.begin(); i != cont.end(); ++i) {
+		if (i != cont.begin()) {
+			cout << c;
+		}
+		cout << *i;
+	}
+}
+
+
+
+int main()
+{
+	list<int> vec = {1,2,3};
+	print(vec, ',');
+	return 0;
+}
